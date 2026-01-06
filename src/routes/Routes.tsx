8fdd78604c06dec01e03cdc045dac123c04f83f9
shawtech2019 <shawtechglobal@yahoo.com>
2026-01-06 23:43:03 +0100

    first commit

---
+++ src/routes/Routes.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+
+
+
+
+       
+
+const AppRoutes: React.FC = () => {
+  return (
+    <Router>
+    {/* <Route path="/" element={<Home />} /> */}
+    </Router>
+  );
+};
+
+export default AppRoutes;
